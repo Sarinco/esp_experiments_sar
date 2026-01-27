a96e2f7321b872ae7c54d3324e75dbef3b802839
Sarinco <deleuxantoine@gmail.com>
2026-01-27 15:49:53 -0500

    adding exercises to get more familiar with make and cmake
    Includes a tutorial series with exercises of cmake found online. Step0
    and 1 are already completed

---
+++ cmake_experiments/classic_makefile/src/main.c
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+#include "hello_world.h"
+
+int main() {
+  printf("This is the main function calling\n");
+  hello();
+  return 0;
+}
---
+++ cmake_experiments/cmake-4.2.3-tutorial-source/Step0/Step0.md
@@ -0,0 +1,11 @@
+
+test step to ensure cmake works
+
+run in terminal:
+```
+cmake -B build
+./build/hello
+```
+
+
+clean the build dir after each use
